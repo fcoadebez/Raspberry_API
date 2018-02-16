26d496ee53b42bd174222aa5388b8a828a4b10ab
ECV <ecv@ECV-BO-B018-IM032.local>
2018-02-16 17:43:15 +0100

    Add Mongo project

---
+++ Mongo/Models/Products.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MVC6_WEBAPI_MongoDB.Models
+{
+    public class Product
+    {
+        public ObjectId Id { get; set; }
+        [BsonElement("machine_id")]
+        public int MachineId { get; set; }
+        [BsonElement("name")]
+        public string Name { get; set; }
+        [BsonElement("current_stock")]
+        public int CurrentStock { get; set; }
+        [BsonElement("max_stock")]
+        public int MaxStock { get; set; }
+        [BsonElement("image_url")]
+        public string ImageUrl { get; set; }
+        [BsonElement("category")]
+        public string Category { get; set; }
+
+    }
+}
