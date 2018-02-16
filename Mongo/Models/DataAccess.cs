08fa097341411380819cba5d1b01dbd45acf33ef
ECV <ecv@ECV-BO-B018-IM032.local>
2018-02-16 18:57:01 +0100

    Add tests project + query one/query all

--- Mongo/Models/DataAccess.cs
+++ Mongo/Models/DataAccess.cs
@@ -1,6 +1,6 @@
 ﻿using MongoDB.Bson;
 using MongoDB.Driver;
-using MongoDB.Driver.Builders;
+using MongoDB.Driver.Linq;
 using System.Collections.Generic;
 
 namespace MVC6_WEBAPI_MongoDB.Models
@@ -8,45 +8,49 @@ namespace MVC6_WEBAPI_MongoDB.Models
     public class DataAccess
     {
         MongoClient _client;
-        MongoServer _server;
-        MongoDatabase _db;
+        IMongoDatabase _db;
 
         public DataAccess()
         {
-            _client = new MongoClient("ds237808.mlab.com:37808/app-iot");
-            _server = _client.GetServer();
-            _db = _server.GetDatabase("Products");
+            _client = new MongoClient("ecv-app-iot:ecv123@ds237808.mlab.com:37808/app-iot");
+            _db = _client.GetDatabase("Products");
         }
 
         public IEnumerable<Product> GetProducts()
         {
-            return _db.GetCollection<Product>("Products").FindAll();
-        }
+            return _db.GetCollection<Product>("Products").AsQueryable<Product>().ToList();
 
+        }
 
         public Product GetProduct(ObjectId id)
         {
-            var res = Query<Product>.EQ(p => p.Id, id);
-            return _db.GetCollection<Product>("Products").FindOne(res);
+            /*var res = Query<Product>.EQ(p => p.Id, id);
+            return _db.GetCollection<Product>("Products").FindOne();*/
+            var filter = Builders<Product>.Filter.Eq("i", id);
+            return _db.GetCollection<Product>("Products").Find(filter).FirstOrDefault();
         }
 
         public Product Create(Product p)
         {
-            _db.GetCollection<Product>("Products").Save(p);
-            return p;
+            /*_db.GetCollection<Product>("Products").Save(p);
+            return p;*/
+            //return _db.GetCollection<Product>("Products").AsQueryable<Product>().ToList();
         }
 
         public void Update(ObjectId id, Product p)
         {
-            p.Id = id;
+            /*p.Id = id;
             var res = Query<Product>.EQ(pd => pd.Id, id);
             var operation = Update<Product>.Replace(p);
-            _db.GetCollection<Product>("Products").Update(res, operation);
+            _db.GetCollection<Product>("Products").Update(res, operation);*/
+            //return _db.GetCollection<Product>("Products").AsQueryable<Product>().ToList();
+
         }
         public void Remove(ObjectId id)
         {
-            var res = Query<Product>.EQ(e => e.Id, id);
-            var operation = _db.GetCollection<Product>("Products").Remove(res);
+            /*var res = Query<Product>.EQ(e => e.Id, id);
+            var operation = _db.GetCollection<Product>("Products").Remove(res);*/
+            //return _db.GetCollection<Product>("Products").AsQueryable<Product>().ToList();
         }
     }
 }
